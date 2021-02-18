37d51b80b9c427f8dbfa048277a5f65893d7b7ca
NazarKremin <nazikys14@gmail.com>
2021-02-18 23:35:46 +0200

    lesson-1

--- app.js
+++ app.js
@@ -26,7 +26,7 @@
 //     });
 // };
 //
-// sort(randomFile);
+// sortFiles(randomFile);
 
 //Normal Task
 
@@ -91,6 +91,7 @@ fs.readdir(dirGenders, (err, files) => {
     files.map(fileName => {
         fs.readFile(path.join(dirGenders, fileName), (err1, data) => {
             console.log(fileName);
+            
             if (JSON.parse(data).gender === "female") {
                 fs.rename(path.join(__dirname, 'dir', 'genders', fileName), path.join(__dirname, 'dir', '1800', fileName), err2 => {
                     if (err2) {
