b9a7d30ed49ed05b034d2a9f52227c5f08d837ab
NazarKremin <nazikys14@gmail.com>
2021-02-18 18:18:58 +0200

    lesson1

--- app.js
+++ app.js
@@ -1,40 +1,89 @@
+//Task*
+// function sortFiles(link) {
+//     fs.readdir(link, (err, files) => {
+//       err ? console.log(err) : null;
+//
+//         files.forEach(fileName => {
+//             const pathFile = path.join(link, fileName)
+//             fs.stat(pathFile, (err1, stats) => {
+//                 if (stats.isDirectory()) {
+//                     console.log(true)
+//                     return sort(pathFile)
+//
+//
+//                 }
+//                 console.log(false)
+//                 const pathSort = path.join(__dirname, 'sortFile', fileName)
+//                 fs.rename(pathFile, pathSort,
+//                     err => {
+//                         if (err) {
+//                             console.log(err);
+//                             return;
+//                         }
+//                     })
+//
+//             })
+//         })
+//     });
+// }
+//
+// sort(randomFile);
+
+
 // const fs = require('fs');
+// const path = require('path');
 //
-// const dirPath1800 = __dirname + '/dir/1800';
-// const dirPath2000 = __dirname + '/dir/2000';
+// const dirGendersFemale = path.join(__dirname,'dir','genders','Karina.txt');
+// const dirGendersMale = path.join(__dirname,'dir','genders','Oleg.txt');
+// const dirGenders = path.join(__dirname,'dir','genders')
 
-// const dirPathKarina2000 = __dirname + '/dir/2000/Karina.txt';
-// const dirPathOleg1800 = __dirname + '/dir/1800/Oleg.txt';
-//
-
-// fs.writeFile(dirPathKarina2000,'{"gender": "female"}',err => {
+// fs.writeFile(dirGendersFemale,'{"gender": "female"}',err => {
 //     err ? console.log(err) : null;
 // });
-// fs.writeFile(dirPathOleg1800,'{"gender": "male"}',err => {
-//     err ? console.log(err) : null;
-// });
-
-
-// fs.readdir(dirPath1800,(err, files) => {
-//     err ? console.log(err) : null;
-//     console.log(files);
 //
-//     files.map(menFiles => {
-//         console.log(menFiles)
-//         fs.rename(`${__dirname}/dir/1800/Oleg.txt`,`${__dirname}/dir/2000/Oleg.txt`,err1 => {
-//             err ? console.log(err) : null;
-//         });
-//     });
-// });
-
-// fs.readdir(dirPath2000,(err, files) => {
+// fs.writeFile(dirGendersMale,'{"gender": "male"}',err => {
 //     err ? console.log(err) : null;
-//     console.log(files);
-//
-//     files.map(womanFiles=>{
-//         console.log(womanFiles);
-//         fs.rename(`${__dirname}/dir/2000/Karina.txt`,`${__dirname}/dir/1800/Karina.txt`,err1 => {
-//             err ? console.log(err) : null;
-//         });
-//     });
 // });
+//
+// fs.readdir(derGenderKarina,(err, files) => {
+//     err ? console.log(err) : null;
+//     console.log(files)
+// })
+// fs.readFile(derGenderKarina, (err, data) => {
+//     if (JSON.parse(data).gender == 'female') {
+//         console.log(JSON.parse(data).gender);
+//     }
+// })
+//
+// fs.readdir(dirGenders,(err, files) => {
+//     err ? console.log(err) : null;
+//     console.log(files)
+//     files.map(fileName=>{
+//         fs.readFile(path.join(dirGenders,fileName),(err1, data) => {
+//             console.log(fileName)
+//             JSON.parse(data).gender === "male" ?  fs.rename(path.join(__dirname,'dir','genders',fileName),path.join(__dirname,'dir','2000',fileName),err1 => {
+//                 err1 ? console.log(err) : null;
+//             }) : null;
+//         })
+//
+//     })
+// })
+//
+// fs.readdir(dirGenders,(err, files) => {
+//     err ? console.log(err) : null;
+//     console.log(files)
+//     files.map(fileName=>{
+//         fs.readFile(path.join(dirGenders,fileName),(err1, data) => {
+//             console.log(fileName)
+//             JSON.parse(data).gender === "female" ?  fs.rename(path.join(__dirname,'dir','genders',fileName),path.join(__dirname,'dir','1800',fileName),err1 => {
+//                 err1 ? console.log(err) : null;
+//             }) : null;
+//         })
+//
+//     })
+// })
+//
+
+
+
+
