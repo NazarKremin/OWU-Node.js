0719643e6ccb8fda7dc02b1df285b85d80d9ecf0
NazarKremin <nazikys14@gmail.com>
2021-03-05 15:47:25 +0200

    homework-6

--- dataBase/models/Tokens.js
+++ dataBase/models/Tokens.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const tokensSchema = new Schema({
     access_token: { type: String },
     refresh_token: { type: String },
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    _user_id: { type: Schema.Types.ObjectId, ref: 'User' },
 }, { timestamps: true });
 
 module.exports = model('Tokens', tokensSchema);
